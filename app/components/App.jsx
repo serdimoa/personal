f7176d04b71371d572895bd5076f523b33583ea7
serdimoa <serdimoa@gmail.com>
2016-09-26 19:38:48 +0500

    about

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import ScrollArea  from 'react-scrollbar';
 import Header from './Header/header';
 import About from './About/about';
 
 function App() {
   return (
-    <div style={{ position: 'relative' }}>
-      <Header />
-      <About />
-    </div>
+    <ScrollArea
+      speed={0.8}
+      className="area"
+      contentClassName="content"
+      horizontal={false}
+    >
+      <div className="content" style={{ position: 'relative' }}>
+        <Header />
+        <About />
+      </div>
+    </ScrollArea>
   );
 }
 
