c55b4394826da1227e2b1c98dbefdf83ca59824b
SerDimoa <serdimoa@gmail.com>
2016-09-28 05:15:41 +0500

    Change arhitecture of portfolio to build project by array

--- app/components/Portfolio/portfolio.jsx
+++ app/components/Portfolio/portfolio.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import PortfolioItem from './portfolio__item';
 
+const skill = [
+  {
+    skill: 'Python, Django, SCSS, HTML5',
+    name: 'pgtpizza.ru',
+    link: 'http://pgtpizza.ru',
+    image: 'pgtpizza.jpg',
+  },
+  {
+    skill: 'Python1, Django1, SCSS1, HTML5',
+    name: 'pgtpizza.ru',
+    link: 'http://pgtpizza.ru',
+    image: 'image',
+  },
+];
+
 function Portfolio() {
   return (
     <div className="portfolio">
-      <PortfolioItem link="#" image="pgtpizza.jpg" name="pgtpizza.ru" />
+      { skill.map((item, key) =>
+        <PortfolioItem
+          key={key}
+          link={item.link}
+          image={item.image}
+          name={item.name}
+          skils={item.skill}
+        />
+      )
+      }
     </div>
   );
 }
--- app/components/Portfolio/portfolio__item.jsx
+++ app/components/Portfolio/portfolio__item.jsx
@@ -1,30 +1,26 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 
 const propTypes = {
   link: PropTypes.string.isRequired,
   image: PropTypes.string,
   name: PropTypes.string,
+  skils: PropTypes.string,
 };
 
 class PortfolioItem extends React.Component {
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(){
-    window.open(this.props.link);
-  }
-
   static methodsAreOk() {
     return true;
   }
 
   render() {
     return (
-      <div className="portfolio__item" onClick={this.handleClick}>
-        <img src={`images/${this.props.image}`} alt={this.props.name} className="portfolio__image"/>
+      <div className="portfolio__item">
+        <a href={this.props.link} rel="noopener noreferrer" target="_blank">
+          <img src={`images/${this.props.image}`} alt={this.props.name} className="portfolio__image" />
+          <h3>{this.props.skils}</h3>
+        </a>
       </div>
+
     );
   }
 }
